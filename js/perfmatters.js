51d1dd10c232d1984bec3e3ec0beb0fb11c14bd5
Letícia Ribeiro Miranda <leticiaribeiro@aluno.unb.br>
2017-06-08 11:02:19 -0300

    minifying js files

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,6 +1,3 @@
-// Measuring the Critical Rendering Path with Navigation Timing
-// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
-
 function logCRP() {
   var t = window.performance.timing,
     dcl = t.domContentLoadedEventStart - t.domLoading,
@@ -12,3 +9,9 @@ function logCRP() {
 window.addEventListener("load", function(event) {
   logCRP();
 });
+
+(function(w,g){w['GoogleAnalyticsObject']=g;
+w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+ga('create', 'UA-XXXX-Y');
+ga('send', 'pageview');
+
