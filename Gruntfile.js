c0241130add9af879ae07f6fea352e485143430e
Letícia Ribeiro <leticiaribeiro@aluno.unb.br>
2017-07-04 10:12:22 -0300

    adding grunt-imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,8 @@ var ngrok = require('ngrok');
 module.exports = function(grunt) {
 
   // Load grunt tasks
-  require('load-grunt-tasks')(grunt);
-
+  //require('load-grunt-tasks')(grunt);
+  
   // Grunt configuration
   grunt.initConfig({
     pagespeed: {
@@ -24,6 +24,37 @@ module.exports = function(grunt) {
         options: {
           strategy: "mobile"
         }
+      }
+    },
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'img/',
+            src: ['**/*.png'],
+            dest: 'img/compressed/',
+            ext: '.png'
+          }
+        ]
+      },
+      jpg: {
+        options: {
+          progressive: true
+        },
+        files: [
+          {
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'img/',
+            src: ['**/*.jpg'],
+            dest: 'img/compressed/',
+            ext: '.jpg'
+          }
+        ]
       }
     }
   });
@@ -43,7 +74,9 @@ module.exports = function(grunt) {
       done();
     });
   });
-
+  
+  grunt.loadNpmTasks('grunt-pagespeed');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   // Register default tasks
-  grunt.registerTask('default', ['psi-ngrok']);
+  grunt.registerTask('default', ['imagemin']);
 }
